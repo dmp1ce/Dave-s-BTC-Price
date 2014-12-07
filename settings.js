85d1095177cf6476a711225a683ee6139d936c70
David Parrish <daveparrish@gmail.com>
2014-12-07 15:14:27 -0500

    Change commission to 15%

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
   // Commission percentage
-  window.mybtcprice.commission = 5
+  window.mybtcprice.commission = 15
 
   // Allow user to change commission?
   window.mybtcprice.commission_locked = true
